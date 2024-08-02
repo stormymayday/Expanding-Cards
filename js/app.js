e4fcb4b8d5f8fc3e18b7100ed2c2b9f6547a22f0
stormymayday <stormymayday7@gmail.com>
2024-08-01 20:15:02 -0700

    Implemented the 'on-click' functionality to add/remove 'active' class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 // Selecting all the divs with class of 'panel'
 const panels = document.querySelectorAll(".panel");
-console.log(panels);
+// console.log(panels);
+
+// Looping through the panel
+panels.forEach((panel) => {
+    // Adding an 'on-click' event listener to each panel
+    panel.addEventListener("click", () => {
+        // Removing the class of 'active' from all panels
+        removeActiveClass();
+
+        // Adding the class of 'active' to the current panel
+        panel.classList.add("active");
+    });
+});
+
+const removeActiveClass = () => {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+};
